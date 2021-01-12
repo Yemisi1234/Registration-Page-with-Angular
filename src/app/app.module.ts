16e1c413219f1b3303553e99a9e472ffa3f7f8c3
unknown <folahanmiyemisi@gmail.com>
2021-01-13 00:50:06 +0800

    fix merge conflict - project working smoothly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 // import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
-=======
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
-
->>>>>>> 4c0b009fb0a2ec1a1148a128541ec4708e83da2a
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,17 +10,12 @@ import { MatCardModule,} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { RouterModule } from '@angular/router';
 import { ResetComponent } from './reset/reset.component';
-<<<<<<< HEAD
 import { NavbarComponent } from './navbar/navbar.component';
-=======
-import { HomeComponent } from './home/home.component';
->>>>>>> 4c0b009fb0a2ec1a1148a128541ec4708e83da2a
 
 
 
@@ -40,11 +27,7 @@ import { HomeComponent } from './home/home.component';
     LoginComponent,
     SignupComponent,
     ResetComponent,
-<<<<<<< HEAD
     NavbarComponent,
-=======
-    HomeComponent,
->>>>>>> 4c0b009fb0a2ec1a1148a128541ec4708e83da2a
   ],
   imports: [
   BrowserModule,
@@ -68,10 +51,5 @@ import { HomeComponent } from './home/home.component';
   bootstrap: [AppComponent],
 
 })
-<<<<<<< HEAD
 export class AppModule {}
 //  platformBrowserDynamic().bootstrapModule(AppModule); 
-=======
-export class AppModule { }
-platformBrowserDynamic().bootstrapModule(AppModule);  
->>>>>>> 4c0b009fb0a2ec1a1148a128541ec4708e83da2a
